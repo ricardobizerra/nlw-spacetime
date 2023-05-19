c3d1d3c14e625187957cfe01bd7d30f9a6fefafc
Ricardo Bizerra <ricardo.bizerra19@gmail.com>
2023-05-18 22:23:39 -0300

    feature(main): front-end code componentization

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,24 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito com {'<3'} and {'</>'} por{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://ricardobizerra.com.br"
+        className="underline hover:text-gray-100"
+      >
+        Ricardo Bizerra
+      </a>{' '}
+      no NLW da{' '}
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://rocketseat.com.br"
+        className="underline hover:text-gray-100"
+      >
+        Rocketseat
+      </a>
+    </div>
+  )
+}
